0753e9d745f14052a45f208bf3e438d57f2764e3
abhishek <avi.garg44@gmail.com>
2016-05-31 03:08:23 +0530

    api interval change

--- app.js
+++ app.js
@@ -19,7 +19,9 @@
             var apiUrl = "https://api.github.com/search/issues?q=is:open+repo:" + reponame;
             // function to get date time according to the given milliseconds
             var last24 = util.convertDate(86400000);
+            console.log(last24);
             var last7 = util.convertDate(604800000);
+            console.log(last7);
 
             var last24Hours = apiUrl + "+created:>" + last24;
             var last24butLess7 = apiUrl + "+created:<" + last24 + "&created:>" + last7;
@@ -27,23 +29,33 @@
 
             // api call for total issues count
             util.getApi(apiUrl).then(function(response) {
+
                 $scope.totalOpenissues = response.data.total_count;
+                $scope.last24butLess7 = parseInt($scope.totalOpenissues) - parseInt($scope.moreThan7days) - parseInt($scope.last24Hours);
+                console.log($scope.last24butLess7);
             });
 
             //api call for Number of open issues that were opened in the last 24 hours
             util.getApi(last24Hours).then(function(response) {
                 $scope.last24Hours = response.data.total_count;
+                $scope.last24butLess7 = parseInt($scope.totalOpenissues) - parseInt($scope.moreThan7days) - parseInt($scope.last24Hours);
+                console.log($scope.last24butLess7);
             });
 
             //api call for - Number of open issues that were opened more than 24 hours ago but less than 7 days ago
-            util.getApi(last24butLess7).then(function(response) {
-                $scope.last24butLess7 = response.data.total_count;
-            })
+            // util.getApi(last24butLess7).then(function(response) {
+            //     $scope.last24butLess7 = response.data.total_count;
+            // });
 
             // api call for - Number of open issues that were opened more than 7 days ago 
             util.getApi(moreThan7days).then(function(response) {
                 $scope.moreThan7days = response.data.total_count;
-            })
+                $scope.last24butLess7 = parseInt($scope.totalOpenissues) - parseInt($scope.moreThan7days) - parseInt($scope.last24Hours);
+                console.log($scope.last24butLess7);
+            });
+
+
+
         }
     }]);
 
@@ -51,12 +63,13 @@
         return {
             // api call
             getApi: function(apiUrl) {
+                console.log(apiUrl);
                 return $http.get(apiUrl);
             },
             // give date in api format from given time
             convertDate: function(num) {
                 var d1 = new Date();
-                var tt = Number(d1) - 86400000;
+                var tt = Number(d1) - num;
                 var d = new Date(tt);
                 var month = (d.getUTCMonth() + 1);
                 if (month <= 9) {
